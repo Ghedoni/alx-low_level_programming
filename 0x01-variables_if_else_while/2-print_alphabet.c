13d26d850f6b8ebd3f0f5b3c6e413cc885604d94
Ghedoni <elemigee@gmail.com>
2022-04-08 17:16:58 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabets
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
