acbbf07a1146587c14a299c8ac0d44b267dc1ba8
Ghedoni <elemigee@gmail.com>
2022-04-08 18:10:39 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
- * main - Prints numbers 0-9 
+ * main - Prints numbers
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int i = 48;
+	int start = 48;
 
-	while (i <= 57)
+	while (start <= 57)
 	{
-		putchar(i);
-		i++;
+		putchar(start);
+		start++;
 	}
 	putchar(10);
 	return (0);
