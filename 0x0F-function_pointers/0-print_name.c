e9bcea35d1ca5a9ccc0eac7a69d04e9c15d11c2e
Ghedoni <elemigee@gmail.com>
2022-05-13 11:40:38 -0700

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - prints names as it is
+ * @name: name of a person
+ * @f: is pointer
+ * Return: Nothing.
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	ese
+	{
+		f(name);
+	}
+}
