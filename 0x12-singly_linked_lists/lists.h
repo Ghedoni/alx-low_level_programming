0987b5a2c10e5cca7d5fab8cea171992da998689
Ghedoni <elemigee@gmail.com>
2022-05-25 12:42:59 -0700

    Commit 1

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,6 +5,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: strings
+ * @len: unsign int for len
+ * @next: link to next node
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+}list_t;
+
 list_t *add_node_end(list_t **head, const char *str);
 list_t *add_node(list_t **head, const char *str);
 size_t print_list(const list_t *h);
