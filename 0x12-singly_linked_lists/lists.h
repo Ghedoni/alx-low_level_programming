84f339828cd9e4bf0641c51c7cb7a55edfe2af09
Ghedoni <elemigee@gmail.com>
2022-05-25 12:29:53 -0700

    Commit 1

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,14 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+list_t *add_node_end(list_t **head, const char *str);
+list_t *add_node(list_t **head, const char *str);
+size_t print_list(const list_t *h);
+void free_list(list_t *head);
+size_t list_len(const list_t *h);
+
+#endif
